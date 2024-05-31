5d0e368c9e8c71a04c30f19d75f83c4b74863010
Danang Wisnu Prabowo <222111975@stis.ac.id>
2024-05-31 11:26:08 +0700

    package: add mantine core, mantine hooks, dan mantine chart racharts

--- src/assets/screens/Statistics.tsx
+++ src/assets/screens/Statistics.tsx
@@ -1,9 +1,54 @@
-import { Title } from "@mantine/core";
+// import { Title } from "@mantine/core";
 import Layout from "../../components/Layout";
 import BarCharts from "../../components/charts/BarCharts";
 import PieCharts from "../../components/charts/PieCharts";
+import { AreaChart, BarChart } from "@mantine/charts";
 
 const Statistics = () => {
+  const data = [
+    {
+      date: "Mar 22",
+      Apples: 110,
+    },
+    {
+      date: "Mar 23",
+      Apples: 60,
+    },
+    {
+      date: "Mar 24",
+      Apples: 80,
+    },
+    {
+      date: "Mar 25",
+      Apples: null,
+    },
+    {
+      date: "Mar 26",
+      Apples: null,
+    },
+    {
+      date: "Mar 27",
+      Apples: 40,
+    },
+    {
+      date: "Mar 28",
+      Apples: 120,
+    },
+    {
+      date: "Mar 29",
+      Apples: 80,
+    },
+  ];
+
+  const tabelData = [
+    { month: "January", Smartphones: 1200, Laptops: 900, Tablets: 200 },
+    { month: "February", Smartphones: 1900, Laptops: 1200, Tablets: 400 },
+    { month: "March", Smartphones: 400, Laptops: 1000, Tablets: 200 },
+    { month: "April", Smartphones: 1000, Laptops: 200, Tablets: 800 },
+    { month: "May", Smartphones: 800, Laptops: 1400, Tablets: 1200 },
+    { month: "June", Smartphones: 750, Laptops: 600, Tablets: 1000 },
+  ];
+
   return (
     <>
       <Layout pageTitle="STATISTIK">
@@ -12,9 +57,33 @@ const Statistics = () => {
             <BarCharts />
             <PieCharts />
           </div>
-          <div className="w-full bg-red-500 h-screen">
-            {/* <Donut /> */}
-            <Title order={1} component={"p"} size={"4rem"}>Test</Title>
+          <div className="w-full h-screen">
+            <AreaChart
+              h={300}
+              data={data}
+              dataKey="date"
+              series={[{ name: "Apples", color: "indigo.6" }]}
+              curveType="linear"
+              connectNulls
+            />
+            <BarChart
+              h={300}
+              data={tabelData}
+              dataKey="month"
+              referenceLines={[
+                {
+                  y: 130,
+                  color: "red.5",
+                  label: "Profit reached",
+                  labelPosition: "insideTopRight",
+                },
+              ]}
+              series={[
+                { name: "Smartphones", color: "violet.6" },
+                { name: "Laptops", color: "blue.6" },
+                { name: "Tablets", color: "teal.6" },
+              ]}
+            />
           </div>
         </div>
       </Layout>
--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,19 @@ import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
+import "@mantine/core/styles.css";
+import "@mantine/charts/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { ThemeProvider } from "./layout/ThemeContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
     <React.StrictMode>
-      <ThemeProvider>
-        <MantineProvider>
+      <MantineProvider>
+        <ThemeProvider>
           <App />
-        </MantineProvider>
-      </ThemeProvider>
+        </ThemeProvider>
+      </MantineProvider>
     </React.StrictMode>
-    ,
   </HelmetProvider>
 );
