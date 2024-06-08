f4ed6eaf01f90bfc71f7154e8a767a9d3892cd2b
Danang Wisnu Prabowo <222111975@stis.ac.id>
2024-06-08 18:45:36 +0700

    fix: displaying data UMKM in small screen

--- src/components/table/MinimalisTableUMKM.tsx
+++ src/components/table/MinimalisTableUMKM.tsx
@@ -1,14 +1,9 @@
 import React, { useEffect, useState } from "react";
 import ArrowSorting from "./ArrowSorting";
-import {
-  UMKMProperties,
-  dataColumnUMKMBuilder,
-  titleSlugType,
-} from "../../DataBuilder";
+import { UMKMProperties } from "../../DataBuilder";
 import Pagination from "./Pagination";
 import { fetchDataByPagination } from "../../utils";
 import { getBadanUsahaColor, getSkalaUsahaColor } from "../../utils/utils";
-import { IconInfoCircle } from "@tabler/icons-react";
 import DataEmpty from "../commons/DataEmpty";
 import { Link } from "react-router-dom";
 
@@ -16,8 +11,7 @@ interface Props {
   dataUmkm: UMKMProperties[];
 }
 
-const TableUMKM: React.FC<Props> = ({ dataUmkm }) => {
-  const headerDataTable: titleSlugType[] = dataColumnUMKMBuilder;
+const MinimalisTableUMKM: React.FC<Props> = ({ dataUmkm }) => {
   const [activeColumn, setActiveColumn] = useState<string>("Price");
   const [sortingColumn, setSortingColumn] = useState<string | null>("Price");
   const [sortingData, setSortingData] = useState<UMKMProperties[]>([]);
@@ -75,30 +69,25 @@ const TableUMKM: React.FC<Props> = ({ dataUmkm }) => {
             <table className="w-full text-left border-separate border-spacing-y-2 lg:border-spacing-y-3 font-inter transform -translate-y-3">
               <thead className="rounded-lg text-black text-xs md:text-sm lg:text-base">
                 <tr className="">
-                  {headerDataTable?.map((item, index) => (
-                    <th
-                      className={`bg-white py-3 lg:py-6 text-black  justify-start whitespace-nowrap ${
-                        item.slug == "index" && "px-4 lg:px-8 xl:pl-8 "
-                      } rounded-bl dark:text-white px-2 dark:bg-black`}
-                      key={index}
-                    >
-                      <div className="bg-white flex items-center justify-start dark:bg-black">
-                        <ArrowSorting
-                          activeColumn={activeColumn}
-                          sortingColumn={sortingColumn}
-                          includes={item?.slug}
-                          onClickFunction={sortByColumn}
-                        />
-                        <span
-                          className="cursor-pointer pl-1"
-                          onClick={() => sortByColumn(item?.slug)}
-                        >
-                          {item?.title}
-                        </span>
-                      </div>
-                    </th>
-                  ))}
-                  <th className="bg-white py-3 pr-4x lg:pr-8x xl:pr-8 justify-center text-black sm:text-base  dark:text-white rounded-br dark:bg-black">
+                  <th
+                    className={`bg-white py-3 lg:py-6 text-black  justify-start whitespace-nowraprounded-bl dark:text-white px-2 dark:bg-black`}
+                  >
+                    <div className="bg-white flex items-center justify-start dark:bg-black">
+                      <ArrowSorting
+                        activeColumn={activeColumn}
+                        sortingColumn={sortingColumn}
+                        includes={"name"}
+                        onClickFunction={sortByColumn}
+                      />
+                      <span
+                        className="cursor-pointer pl-1"
+                        onClick={() => sortByColumn("name")}
+                      >
+                        UMKM{" "}
+                      </span>
+                    </div>
+                  </th>
+                  <th className="bg-white py-3 pr-4x lg:pr-8x xl:pr-3xl justify-center text-black sm:text-base  dark:text-white rounded-br dark:bg-black">
                     Tindakan
                   </th>
                 </tr>
@@ -107,55 +96,38 @@ const TableUMKM: React.FC<Props> = ({ dataUmkm }) => {
                 {paginatedUMKM.map((data, index) => (
                   <tr
                     key={index}
-                    className="bg-white dark:bg-black text-sm lg:text-base"
+                    className="bg-white dark:bg-black text-sm lg:text-base px-4"
                   >
-                    <td className="py-4 lg:py-5 whitespace-nowrap px-2 lg:px-4 xl:pl-xl font-bold dark:border-slate-700 rounded-tl rounded-bl text-center">
-                      {data?.index}
-                    </td>
-                    <td className="px-2 lg:px-3 whitespace-normal font-normal  dark:border-slate-700">
-                      <div className="flex flex-row items-center w-full gap-2 lg:gap-3">
-                        <img
-                          src={data.avatar}
-                          alt={data.name}
-                          className="rounded-full max-w-10 dark:bg-white"
-                        />
-                        {data?.name}
+                    <td className="py-4 lg:py-5 whitespace-nowrap px-2 lg:px-8 xl:pl-3xl dark:border-slate-700 rounded-tl rounded-bl text-center">
+                      <div className="flex w-full flex-col items-start gap-1 font-normal">
+                        <p className="font-semibold text-sm">{data.name}</p>
+                        <div className="flex flex-row gap-2">
+                          <span
+                            className={`${getSkalaUsahaColor(data?.skala).bg} ${
+                              getSkalaUsahaColor(data?.skala).text
+                            } text-xs font-medium me-2 px-2.5 py-0.5 rounded`}
+                          >
+                            {data?.skala}
+                          </span>
+                          <span
+                            className={`${
+                              getBadanUsahaColor(data?.badanHukum).bg
+                            } ${
+                              getBadanUsahaColor(data?.badanHukum).text
+                            } text-xs font-medium me-2 px-2.5 py-0.5 rounded`}
+                          >
+                            {data?.badanHukum}
+                          </span>
+                        </div>
+                        <p>{data.bidang}</p>
+                        <p>{data.pengampu}</p>
+                        <p className="whitespace-normal">{data.alamat}</p>
                       </div>
                     </td>
-                    <td className="px-2 lg:px-3 whitespace-nowrap font-normal  dark:border-slate-700">
-                      <span
-                        className={`${getSkalaUsahaColor(data?.skala).bg} ${
-                          getSkalaUsahaColor(data?.skala).text
-                        } text-xs lg:text-sm font-medium me-2 px-2.5 py-0.5 rounded`}
-                      >
-                        {data?.skala}
-                      </span>
-                    </td>
-                    <td className="px-2 lg:px-3 whitespace-normal font-normal dark:border-slate-700">
-                      {data?.bidang}
-                    </td>
-                    <td className="px-2 lg:px-3 whitespace-nowrap font-normal dark:border-slate-700">
-                      <span
-                        className={`${
-                          getBadanUsahaColor(data?.badanHukum).bg
-                        } ${
-                          getBadanUsahaColor(data?.badanHukum).text
-                        } text-xs lg:text-sm font-medium me-2 px-2.5 py-0.5 rounded`}
-                      >
-                        {data?.badanHukum}
-                      </span>
-                    </td>
-                    <td className="px-2 lg:px-3  whitespace-normal font-normal dark:border-slate-700">
-                      {data?.pengampu}
-                    </td>
-                    <td className="py-2 px-3  font-normal min-w-[15rem] dark:border-slate-700">
-                      {data?.alamat}
-                    </td>
-                    <td className="pr-4x lg:pr-8x xl:pr-8 dark:border-slate-700 rounded-tr rounded-br">
+                    <td className="pr-4x dark:border-slate-700 rounded-tr rounded-br">
                       <Link to={"/data-umkm/detail"}>
                         <button className="flex flex-row gap-2 items-center text-xs lg:text-sm bg-primary hover:bg-secondaryHover px-2 py-1 rounded text-white transition duration-300">
                           Detail
-                          <IconInfoCircle size={17} />
                         </button>
                       </Link>
                     </td>
@@ -190,4 +162,4 @@ const TableUMKM: React.FC<Props> = ({ dataUmkm }) => {
     </div>
   );
 };
-export default TableUMKM;
+export default MinimalisTableUMKM;
