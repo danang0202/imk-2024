3f1daae586e0fcaef47c8ce66190bbeea7196f9c
222112243 <222112243@stis.ac.id>
2024-06-13 14:46:34 +0700

    regis-5

--- src/assets/screens/Register.tsx
+++ src/assets/screens/Register.tsx
@@ -12,13 +12,13 @@ const Register = () => {
     <div className="flex justify-center items-center min-h-screen bg-silver">
         <div className="bg-white rounded-md shadow-lg overflow-hidden w-11/12 lg:w-2/3">
             
-            <div className="p-8 flex flex-col xl:px-10">
-                <h2 className="text-2xl xl:text-3xl font-bold  text-black text-center">Pendaftaran</h2>
+            <div className="p-5 flex flex-col xl:px-10">
+                <h2 className="text-2xl xl:text-3xl font-bold text-black text-center">Pendaftaran</h2>
                 <p className="font-extralight text-lg xl:text-xl text-center mb-8">e-UMKM</p>
-                <form className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-4">
+                <form className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-3">
                     {/* <!-- Kolom Kiri --> */}
                     <div className='px-4'>
-                        <div className="relative w-full mb-10 group">
+                        <div className="relative w-full mb-8 group">
                             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none z-10">
                                 <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-building-store opacity-55"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 21l18 0" /><path d="M3 7v1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1m0 1a3 3 0 0 0 6 0v-1h-18l2 -4h14l2 4" /><path d="M5 21l0 -10.15" /><path d="M19 21l0 -10.15" /><path d="M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4" /></svg>
                             </div>
@@ -38,7 +38,7 @@ const Register = () => {
                             </label>
                         </div>                  
 
-                        <div className="relative w-full mb-10 group">
+                        <div className="relative w-full mb-8 group">
                             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none z-10">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="opacity-55 icon icon-tabler icons-tabler-outline icon-tabler-arrows-maximize"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M16 4l4 0l0 4" /><path d="M14 10l6 -6" /><path d="M8 20l-4 0l0 -4" /><path d="M4 20l6 -6" /><path d="M16 20l4 0l0 -4" /><path d="M14 14l6 6" /><path d="M8 4l-4 0l0 4" /><path d="M4 4l6 6" /></svg>
                             </div>
@@ -63,7 +63,7 @@ const Register = () => {
                         </div>
 
                         
-                        <div className="relative w-full mb-10 group">
+                        <div className="relative w-full mb-8 group">
                             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none z-10">
                             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="opacity-55 icon icon-tabler icons-tabler-outline icon-tabler-layout-grid"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M14 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M4 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /><path d="M14 14m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" /></svg>
                             </div>
@@ -98,7 +98,7 @@ const Register = () => {
                             </label>
                         </div>
 
-                        <div className="relative w-full mb-10 group">
+                        <div className="relative w-full mb-8 group">
                             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none z-10">
                             <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="opacity-55 icon icon-tabler icons-tabler-outline icon-tabler-gavel"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M13 10l7.383 7.418c.823 .82 .823 2.148 0 2.967a2.11 2.11 0 0 1 -2.976 0l-7.407 -7.385" /><path d="M6 9l4 4" /><path d="M13 10l-4 -4" /><path d="M3 21h7" /><path d="M6.793 15.793l-3.586 -3.586a1 1 0 0 1 0 -1.414l2.293 -2.293l.5 .5l3 -3l-.5 -.5l2.293 -2.293a1 1 0 0 1 1.414 0l3.586 3.586a1 1 0 0 1 0 1.414l-2.293 2.293l-.5 -.5l-3 3l.5 .5l-2.293 2.293a1 1 0 0 1 -1.414 0z" /></svg>
                             </div>
@@ -122,7 +122,7 @@ const Register = () => {
                             </label>
                         </div>
 
-                        <div className="relative w-full mb-10 group">
+                        <div className="relative w-full mb-8 group">
                             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none z-10">
                             <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="opacity-55 icon icon-tabler icons-tabler-outline icon-tabler-user-pentagon"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M13.163 2.168l8.021 5.828c.694 .504 .984 1.397 .719 2.212l-3.064 9.43a1.978 1.978 0 0 1 -1.881 1.367h-9.916a1.978 1.978 0 0 1 -1.881 -1.367l-3.064 -9.43a1.978 1.978 0 0 1 .719 -2.212l8.021 -5.828a1.978 1.978 0 0 1 2.326 0z" /><path d="M12 13a3 3 0 1 0 0 -6a3 3 0 0 0 0 6z" /><path d="M6 20.703v-.703a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v.707" /></svg>
                             </div>
@@ -151,8 +151,8 @@ const Register = () => {
                             </label>
                         </div>
 
-                        <div className="relative w-full mb-8 group">
-                        <div className="absolute inset-y-0 left-0 flex pt-3 pl-3 pointer-events-none z-10">
+                        <div className="relative w-full mb-0 group">
+                            <div className="absolute inset-y-0 left-0 flex pt-3 pl-3 pointer-events-none z-10">
                                 <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="opacity-55 icon icon-tabler icons-tabler-outline icon-tabler-map-pin"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /><path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" /></svg>
                             </div>
                             <textarea
@@ -173,7 +173,7 @@ const Register = () => {
 
                     {/* <!-- Kolom Kanan --> */}
                     <div className='px-4'>
-                        <div className="relative w-full mb-10 group">
+                        <div className="relative w-full mb-8 group">
                             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none z-10">
                                 <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-phone opacity-55"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" /></svg>
                             </div>
@@ -192,13 +192,13 @@ const Register = () => {
                                 Nomor Telepon
                             </label>
                         </div>
-                        
-                        <div className="relative w-full mb-10 group">
+
+                        <div className="relative w-full mb-8 group">
                             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none z-10">
                                 <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-mail opacity-55"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" /><path d="M3 7l9 6l9 -6" /></svg>
                             </div>
                             <input
-                                type="email"
+                                type="text"
                                 name="email"
                                 id="email"
                                 className="block py-2 lg:py-3 pl-10 w-full text-sm md:text-base text-grey bg-silver rounded-md border-none appearance-none focus:outline-none focus:ring-0 peer"
@@ -213,7 +213,7 @@ const Register = () => {
                             </label>
                         </div>
 
-                        <div className="relative w-full mb-10 group">
+                        <div className="relative w-full mb-8 group">
                             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none z-10">
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-user opacity-55">
@@ -238,7 +238,7 @@ const Register = () => {
                             </label>
                         </div>
 
-                        <div className="relative w-full mb-10 group">
+                        <div className="relative w-full mb-8 group">
                             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none z-10">
                                 <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="opacity-50 icon icon-tabler icons-tabler-outline icon-tabler-lock">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -270,7 +270,7 @@ const Register = () => {
                             </div>
                         </div>
 
-                        <div className="relative w-full mb-8 group">
+                        <div className="relative w-full mb-6 group">
                         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none z-10">
                                 <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="opacity-50 icon icon-tabler icons-tabler-outline icon-tabler-lock">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -302,17 +302,28 @@ const Register = () => {
                             </div>
                         </div>
 
-                        <div className="flex items-center">
+                        <div className="flex items-center mb-2">
                             <div className="h-5 mr-2 ">
                                 <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-silver rounded bg-silver focus:ring-3 cursor-pointer" required />
                             </div>
                             <label htmlFor="remember" className="text-sm text-greyDark cursor-pointer">Saya menerima syarat dan ketentuan yang berlaku</label>
                         </div>
 
-                        <button className="w-full py-2 bg-primary text-white rounded-md font-bold mt-4 ">
+                        <button className="w-full py-2 bg-primary text-white rounded-md font-semibold md:font-bold mt-4 ">
                             Daftar
                         </button>
                     </div>
+
+                    <div className="mt-0 text-sm flex justify-start items-center mb-0 py-0 ml-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-left opacity-70">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M5 12l14 0" />
+                        <path d="M5 12l4 4" />
+                        <path d="M5 12l4 -4" />
+                        </svg>
+                        <a href="/beranda" className="text-black py-1">Kembali ke Home</a>
+                    </div>
+
                 </form>
             </div>
         </div>
