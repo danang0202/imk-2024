ec061323d284225064c47f68d3017435a12b4425
hanunnab <134913120+hanunnab@users.noreply.github.com>
2024-06-13 19:50:11 +0700

    Add files via upload
    arrow di info modal tapi masih tempelan

---
+++ src/components/table/ArrowHanun.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface ArrowHanunProps {
+  active: boolean;
+}
+
+const ArrowHanun: React.FC<ArrowHanunProps> = ({ active }) => {
+  return (
+    <svg
+      className="w-4 h-4 cursor-pointer text-inactive group-hover:text-black rotate-180 mr-2"
+      xmlns="http://www.w3.org/2000/svg"
+      width="100"
+      height="100"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M19 14l-7 7m0 0l-7-7m7 7V3"
+      />
+    </svg>
+  );
+};
+
+export default ArrowHanun;
